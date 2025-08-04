0ce6e435f4b4e002e9c49b26e463665f9817a55c
Giulia Galizoni Caversan <ggalizon@c3a10c2.42berlin.de>
2025-08-04 17:34:42 +0200

    main.c norminetted

--- philo/src/main.c
+++ philo/src/main.c
@@ -1,26 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggalizon <ggalizon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/04 17:33:49 by ggalizon          #+#    #+#             */
+/*   Updated: 2025/08/04 17:33:50 by ggalizon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/philosophers.h"
-
-
 
 static void	init_table_struct(t_table *table)
 {
-		table->philo_number = 0;
-		table->time_to_die = 0;
-		table->time_to_eat = 0;
-		table->time_to_sleep = 0;
-		table->nbr_limit_meals = 0;
-		table->start_simulation = 0;
-		table->end_simulation = 0;
-		table->all_ready = 0;
-		table->running_threads = 0;
-		table->philos = NULL;
-		table->forks = NULL;
+	table->philo_number = 0;
+	table->time_to_die = 0;
+	table->time_to_eat = 0;
+	table->time_to_sleep = 0;
+	table->nbr_limit_meals = 0;
+	table->start_simulation = 0;
+	table->end_simulation = 0;
+	table->all_ready = 0;
+	table->running_threads = 0;
+	table->philos = NULL;
+	table->forks = NULL;
 }
 
 int	main(int ac, char **av)
 {
-	t_table table;
-	int	status;
+	t_table	table;
+	int		status;
 
 	init_table_struct(&table);
 	status = 0;
@@ -32,5 +42,5 @@ int	main(int ac, char **av)
 		status = 1;
 	if (!cleanup(&table))
 		status = 1;
-	return status;
+	return (status);
 }
