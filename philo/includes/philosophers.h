a2edc627714938b2218b22831ed3437e69f85a55
Giulia Galizoni Caversan <ggalizon@c3a10c2.42berlin.de>
2025-08-04 17:56:42 +0200

    philosophers.h now really norminetted

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -1,5 +1,17 @@
-#ifndef PHILO_H
-# define PHILO_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggalizon <ggalizon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/04 17:45:33 by ggalizon          #+#    #+#             */
+/*   Updated: 2025/08/04 17:49:51 by ggalizon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
 # include <pthread.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -47,65 +59,64 @@ typedef struct s_fork
 //Philo
 typedef struct s_philo
 {
-	int		id;
-	long	meals_counter;
-	int		is_full;
-	long	last_meal_time;
-	t_fork	*first_fork;
-	t_fork	*second_fork;
-	pthread_t	thread_id;
-	pthread_mutex_t lock;
-	t_table	*table;
+	int				id;
+	long			meals_counter;
+	int				is_full;
+	long			last_meal_time;
+	t_fork			*first_fork;
+	t_fork			*second_fork;
+	pthread_t		thread_id;
+	pthread_mutex_t	lock;
+	t_table			*table;
 }	t_philo;
 
 struct s_table
 {
-	int	philo_number;
-	long	time_to_die;
-	long	time_to_eat;
-	long	time_to_sleep;
-	int	nbr_limit_meals;
-	long	start_simulation;
-	int		end_simulation;
-	int		all_ready;
-	int	running_threads;
-	pthread_t	monitor;
+	int				philo_number;
+	long			time_to_die;
+	long			time_to_eat;
+	long			time_to_sleep;
+	int				nbr_limit_meals;
+	long			start_simulation;
+	int				end_simulation;
+	int				all_ready;
+	int				running_threads;
+	pthread_t		monitor;
 	pthread_mutex_t	lock;
-	pthread_mutex_t write_lock;
-	t_fork	*forks;
-	t_philo	*philos;
+	pthread_mutex_t	write_lock;
+	t_fork			*forks;
+	t_philo			*philos;
 };
 
-int	ft_atoi(const char *str);
-int data_init(char **av, t_table *table);
-int	check_input(int ac, char **av);
-int	start(t_table *table);
+int		ft_atoi(const char *str);
+int		data_init(char **av, t_table *table);
+int		check_input(int ac, char **av);
+int		start(t_table *table);
 void	*safe_malloc(size_t size);
-int	safe_mutex_handle(pthread_mutex_t *mutex, t_opcode opcode);
-int	safe_thread_handle(pthread_t *thread, void *(*function)(void *), void *data, t_opcode opcode);
+int		safe_mutex_handle(pthread_mutex_t *mutex, t_opcode opcode);
+int		safe_thread_handle(pthread_t *thread, void *(*function)(void *),
+			void *data, t_opcode opcode);
 
-int	write_action(t_philo_action action, t_philo *philo);
+int		write_action(t_philo_action action, t_philo *philo);
 
 // safety helpers
-int	set_int(pthread_mutex_t *mutex, int *dest, int value);
-int	get_int(pthread_mutex_t *mutex, int *value, int *dest);
-int	increase_int(pthread_mutex_t *mutex, int *value);
-int	set_long(pthread_mutex_t *mutex, long *dest, long value);
-int	get_long(pthread_mutex_t *mutex, long *value, long *dest);
+int		set_int(pthread_mutex_t *mutex, int *dest, int value);
+int		get_int(pthread_mutex_t *mutex, int *value, int *dest);
+int		increase_int(pthread_mutex_t *mutex, int *value);
+int		set_long(pthread_mutex_t *mutex, long *dest, long value);
+int		get_long(pthread_mutex_t *mutex, long *value, long *dest);
 
-
-int	finish_simulation(t_table *table, int *is_finished);
+int		finish_simulation(t_table *table, int *is_finished);
 long	get_time(t_time_code timecode);
-int	ft_usleep(long usec, t_table *table);
+int		ft_usleep(long usec, t_table *table);
 
 void	*monitor(void *data);
 
-int	cleanup(t_table *table);
+int		cleanup(t_table *table);
 
 // action routines
-int	force_think(t_philo *philo);
-int	think(t_philo *philo);
-int eat(t_philo *philo);
-
+int		force_think(t_philo *philo);
+int		think(t_philo *philo);
+int		eat(t_philo *philo);
 
 #endif
