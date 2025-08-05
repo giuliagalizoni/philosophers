7ee43952010b2b6bc39072908d239718f40a6f46
Giulia <96666886+giuliagalizoni@users.noreply.github.com>
2025-08-05 14:30:58 +0000

    refactor write function

--- philo/src/write.c
+++ philo/src/write.c
@@ -1,26 +1,10 @@
 #include "../includes/philosophers.h"
 
-int	write_action(t_philo_action action, t_philo *philo)
+void	print_actions(t_philo_action action, t_philo *philo)
 {
-	long elapsed;
-	int	is_finished;
-	int	is_full;
+	long	elapsed;
 
-	is_finished = 0;
-	is_full = 0;
-	if (!safe_mutex_handle(&philo->table->write_lock, LOCK))
-		return (0);
 	elapsed = get_time(MILISECOND) - philo->table->start_simulation;
-
-	if (!get_int(&philo->table->lock, &philo->table->end_simulation, &is_finished)
-		|| !get_int(&philo->lock, &philo->is_full, &is_full))
-		return (0);
-	if(is_finished || is_full)
-	{
-		if (!safe_mutex_handle(&philo->table->write_lock, UNLOCK))
-			return (0);
-		return (1);
-	}
 	if (action == TAKE_FIRST_FORK || action == TAKE_SECOND_FORK)
 		printf("%ld %d has taken a fork\n", elapsed, philo->id);
 	else if (action == EATING)
@@ -31,6 +15,31 @@ int	write_action(t_philo_action action, t_philo *philo)
 		printf("%ld %d is thinking\n", elapsed, philo->id);
 	else if (action == DIED)
 		printf("%ld %d died\n", elapsed, philo->id);
+}
+
+int	write_action(t_philo_action action, t_philo *philo)
+{
+	int		is_finished;
+	int		is_full;
+
+	is_finished = 0;
+	is_full = 0;
+	if (!safe_mutex_handle(&philo->table->write_lock, LOCK))
+		return (0);
+	if (!get_int(&philo->table->lock, &philo->table->end_simulation,
+			&is_finished)
+		|| !get_int(&philo->lock, &philo->is_full, &is_full))
+	{
+		safe_mutex_handle(&philo->table->write_lock, UNLOCK);
+		return (0);
+	}
+	if (is_finished || is_full)
+	{
+		if (!safe_mutex_handle(&philo->table->write_lock, UNLOCK))
+			return (0);
+		return (1);
+	}
+	print_actions(action, philo);
 	if (!safe_mutex_handle(&philo->table->write_lock, UNLOCK))
 		return (0);
 	return (1);
